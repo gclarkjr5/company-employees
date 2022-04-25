c3e9002230a1bdb359dd9f80dcb107d2889ae6d3
gclarkjr5 <gclarkjr5@gmail.com>
2022-04-25 05:46:22 -0500

    adding workflow and testing

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+mod employee_action;
+
+// use employee_action::Company;
+// use std::collections::HashMap;
+
+// mod employee_action::add_employee;
+
+
+
+// fn main() {
+//     let mut company = Company {
+//         employee_list: HashMap::new()
+//     };
+
+//     add_employee(&mut company, "gary".to_string(), "sales".to_string());
+//     add_employee(&mut company, "aleks".to_string(), "sales".to_string());
+// }
